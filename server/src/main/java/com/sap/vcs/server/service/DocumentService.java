f50eb9002b7fffb21188a09c5c0834176f15c889
AlexM064 <a8778m@gmail.com>
2026-03-30 00:33:08 +0300

    feat: set document owner and add ownership validation for updates

--- server/src/main/java/com/sap/vcs/server/service/DocumentService.java
+++ server/src/main/java/com/sap/vcs/server/service/DocumentService.java
@@ -1,10 +1,6 @@
 package com.sap.vcs.server.service;
 
-import com.sap.vcs.server.dto.DocumentHistoryResponseDto;
-import com.sap.vcs.server.dto.DocumentRequestDto;
-import com.sap.vcs.server.dto.DocumentResponseDto;
-import com.sap.vcs.server.dto.DocumentVersionResponseDto;
-import com.sap.vcs.server.dto.UpdateDocumentMetadataRequestDto;
+import com.sap.vcs.server.dto.*;
 import com.sap.vcs.server.entity.Document;
 import com.sap.vcs.server.entity.DocumentVersion;
 import com.sap.vcs.server.entity.User;
