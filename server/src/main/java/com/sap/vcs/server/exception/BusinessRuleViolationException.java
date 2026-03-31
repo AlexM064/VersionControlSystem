b3c6225d0766b4ecd60e719d2eaec86bb095efc3
Kristiayn <kristodd2701@gmail.com>
2026-03-31 14:50:10 +0300

    feat: improve global exception handling and error responses

--- server/src/main/java/com/sap/vcs/server/exception/BusinessRuleViolationException.java
+++ server/src/main/java/com/sap/vcs/server/exception/BusinessRuleViolationException.java
@@ -1,7 +1,6 @@
 package com.sap.vcs.server.exception;
 
 public class BusinessRuleViolationException extends RuntimeException {
-
     public BusinessRuleViolationException(String message) {
         super(message);
     }
