6c158bddcf853caf5de40d9210125440f40e1ae6
AlexM064 <a8778m@gmail.com>
2026-04-04 13:26:20 +0300

    Add versioned token issuance endpoint

---
+++ server/src/main/java/com/sap/vcs/server/controller/TokenController.java
@@ -0,0 +1,27 @@
+package com.sap.vcs.server.controller;
+
+import com.sap.vcs.server.dto.auth.AuthResponseDto;
+import com.sap.vcs.server.dto.auth.LoginRequestDto;
+import com.sap.vcs.server.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/tokens")
+@Tag(name = "Tokens", description = "Token issuance endpoints")
+public class TokenController {
+
+    private final AuthService authService;
+
+    public TokenController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping
+    @Operation(summary = "Issue JWT access token")
+    public AuthResponseDto issueToken(@Valid @RequestBody LoginRequestDto request) {
+        return authService.login(request);
+    }
+}
